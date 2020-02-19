a55c62adcbce1634b9105f839fe0624e43cc8cce
Keathan Fertig <keathanf@gmail.com>
2020-02-19 11:31:42 -0600

    Added POST endpoint to WebController, allowing user to specify the message to return in HelloWorldResponse

--- src/main/java/com/charter/hello/world/model/HelloWorldResponse.java
+++ src/main/java/com/charter/hello/world/model/HelloWorldResponse.java
@@ -3,14 +3,20 @@ package com.charter.hello.world.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Component
 public class HelloWorldResponse {
     String message;
 
-    public HelloWorldResponse(String message){
-        this.message = message;
+    public HelloWorldResponse() {
+        this.message = "Hello World!";
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/com/charter/hello/world/web/WebController.java
+++ src/main/java/com/charter/hello/world/web/WebController.java
@@ -1,16 +1,28 @@
 package com.charter.hello.world.web;
 
 import com.charter.hello.world.model.HelloWorldResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController(value = "/hello")
 public class WebController {
-    @GetMapping(value = "/hello",
-            consumes = {"application/json"},
+
+    HelloWorldResponse helloWorldResponse;
+
+    @Autowired
+    WebController(HelloWorldResponse helloWorldResponse) {
+        this.helloWorldResponse = helloWorldResponse;
+    }
+
+    @GetMapping(consumes = {"application/json"},
             produces = {"application/json"})
-    @ResponseBody HelloWorldResponse hello(){
-        return new HelloWorldResponse("Hello World!");
+    @ResponseBody HelloWorldResponse hello() {
+        return helloWorldResponse;
+    }
+
+    @PostMapping(consumes = {"application/json"},
+            produces = {"application/json"})
+    void hello(@RequestBody HelloWorldResponse helloWorldResponse) {
+        this.helloWorldResponse = helloWorldResponse;
     }
 }
